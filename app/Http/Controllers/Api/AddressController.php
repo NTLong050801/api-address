b3de1dbd84b5e79e6ba13d1246e469e830573a96
nguyen thanh long <loglog050801>
2023-10-17 09:00:39 +0700

    remove function not use

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -8,43 +8,8 @@ use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return Address::getByDepth();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(int $parentId = null, int $depth = 0, string $countryCode = 'vn')
+    public function index(int $parentId = null, int $depth = 0, string $countryCode = 'vn')
     {
         return Address::getByDepth($parentId,$depth,$countryCode);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(int $parentId, int $depth, string $countryCode = 'vn')
-    {
-       return Address::getByDepth($parentId,$depth,$countryCode);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
