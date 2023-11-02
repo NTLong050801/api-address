6f0393eaba5df29872c612583c06cfaf9ad0803d
nguyen thanh long <loglog050801>
2023-11-02 13:57:38 +0700

    update envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,10 @@
+@servers(['web' => ['user@45.76.181.29']])
+
+@task('deploy', ['on' => 'web'])
+cd /var/www/api-address
+git pull origin master
+composer install --optimize-autoloader --no-dev
+php artisan cache:clear
+php artisan view:cache
+php artisan optimize
+@endtask
